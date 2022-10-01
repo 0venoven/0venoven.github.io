8a1cc7e4f887a36b6bc8bd70bab3fd4ccd16d56e
Ivan Lim <maskscar123@gmail.com>
2022-10-01 12:12:44 +0800

    switch js code to js file

---
+++ magic.js
@@ -0,0 +1,9 @@
+function changeAppearanceSubmit(){
+    myBtn = document.getElementById("Submit_Form_Button")
+    myBtn.innerText = "Thank You!"
+}
+
+function changeBackAppearanceSubmit(){
+    myBtn = document.getElementById("Submit_Form_Button")
+    myBtn.innerText = "Submit"
+}
